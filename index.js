36c4519b6e36907922a16d34b70e46112a08a24a
BArbosaJJT <143296852+BArbosaJJT@users.noreply.github.com>
2025-05-15 12:30:59 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function Obtener_datos() {
+  let n1 = parseFloat(document.getElemetnById("num1").value);
+  let n2 = parseFloat(document.getElemetnById("num2").value);
+  return [n1, n2];
+}
+function sumar() {
+  const [n1, n2] = Obtener_datos();
+  let r_suma = n1 + n2;
+  document.getElemetnById("resultado").innerText =
+    "Resultado de la Suma es: " + r_suma;
+}
