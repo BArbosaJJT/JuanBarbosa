424bd050335c4a49304653ba8fb5ff518ed63471
BArbosaJJT <143296852+BArbosaJJT@users.noreply.github.com>
2025-05-22 12:28:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ function Obtener_datos() {
 
 function sumar() {
   const [n1, n2] = Obtener_datos();
-  let r_suma = n1 + n2;
   document.getElementById("resultado").innerText =
-    "Resultado de la Suma es: " + r_suma;
+    "Resultado de la Suma es: " + (n1 + n2);
 }
+
+function restar() {
+  const [n1, n2] = Obtener_datos();
+  document.getElementById("resultado").innerText =
+    "Resultado de la Resta es: " + (n1 - n2);
+}
+
+function multiplicar() {
+  const [n1, n2] = Obtener_datos();
+  document.getElementById("resultado").innerText =
+    "Resultado de la Multiplicación es: " + n1 * n2;
+}
+
+function dividir() {
+  const [n1, n2] = Obtener_datos();
+  if (n2 === 0) {
+    document.getElementById("resultado").innerText =
+      "Error: No se puede dividir entre cero.";
+  } else {
+    document.getElementById("resultado").innerText =
+      "Resultado de la División es: " + n1 / n2;
+  }
+}
+
 
